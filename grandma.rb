edf637e400b4c9f0e4d7d2a3b4f3e36e40342e77
Joshua Crum <joshua.crum@gmail.com>
2019-04-28 02:08:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,7 @@
 def speak_to_grandma(words)
-  if words == words.upcase
+  if words == "I LOVE YOU, GRANDMA!"
+    "I LOVE YOU TOO, SONNY!"
+  elsif words == words.upcase
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP SONNY!"
