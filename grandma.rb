79f93423887fa2d855a658240a3db25084fc229a
Joshua Crum <joshua.crum@gmail.com>
2019-04-28 02:11:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(words)
-  if words == "I LOVE YOU, GRANDMA!"
-    "I LOVE YOU TOO,  PUMPKIN!"
+  if words == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif words == words.upcase
     "NO, NOT SINCE 1938!"
   else
